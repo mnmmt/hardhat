a580c4fad872cd091b0861a1d88348ef78c63031
Chris McCormick <chris@mccormick.cx>
2018-03-10 21:32:20 +0800

    Design.

--- src/xmp.cljs
+++ src/xmp.cljs
@@ -12,17 +12,38 @@
 (def re-line #"^.*([A-Z0-9]{2})\/([A-Z0-9]{2})\] Chn\[")
 (def re-bpm #"Speed\[(.*?)\] BPM\[(.*?)\]")
 
+; TODO:
+;  * TC - automount USB
+;  * list all modules on argv paths
+;  * LCD UI
+;  * sync signal
+
+; UI design:
+;
+; > module 1.xm
+;   module 2.it
+;
+; > all on
+;   ch 1  on
+;
+; > ch 3  on
+;   ch 4  off
+
 (let [p (node-pty/spawn "./xmp-wrap" #js ["-l" "./test.it"] process/env)]
   (.on p "data"
        (fn [data]
          (let [[match-bpm subticks rate] (re-find re-bpm data)
                [match-tick tick len] (re-find re-line data)]
-           (if match-bpm (println "---> BPM:" subticks (js/parseInt rate 16)))
-           (if match-tick (println "---> Match:" tick len))
            ;(println "---> LINE:" data)
-           
-           )))
-  ;(p.write "12")
-  )
+           (if match-bpm (println "---> BPM:" subticks (js/parseInt rate 16)))
+           (if match-tick (println "---> Match:" tick len))))))
 
+; toggle channel
+; (p.write "1")
+
+; unmute all
+; (p.write "!")
+
+; toggle pause
+; (p.write " ")
 
