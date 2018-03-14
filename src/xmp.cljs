a9a7d3b2c7533d82f8321046ed3804837d992233
Chris McCormick <chris@mccormick.cx>
2018-03-14 17:46:07 +0800

    Update shim with real lcd plate code.

--- src/xmp.cljs
+++ src/xmp.cljs
@@ -28,8 +28,17 @@
 (def gpio (try (.-Gpio (js/require "onoff"))
             (catch :default e nil)))
 
+(def lcd (try (let [plate (.-plate (js/require "adafruit-i2c-lcd"))]
+                (plate. 1 0x20))
+              (catch :default e nil)))
+
+(when lcd
+  ; turn on sainsmart 1602 I2C backlight
+  (.sendBytes lcd 0 0x1F))
+
 (console.log "xmp.cljs")
 
+; terminal codes to clear the screen
 (def clear-screen (.toString (js/Buffer. #js [27 91 72 27 91 50 74])))
 
 (def re-line #"^.*([A-Z0-9]{2})\/([A-Z0-9]{2})\] Chn\[")
@@ -46,9 +55,17 @@
       (.split (.toString (child_process/execSync find-cmd)) "\n"))
     []))
 
-(defn lcd-print-shim [txt]
-  (print clear)
-  (print txt))
+(defn lcd-print [messages]
+  (let [txt (clojure.string/join "\n" messages)]
+    (if lcd
+      (do
+        (.clear lcd)
+        (.message lcd txt))
+      (do
+        (print clear-screen)
+        (print txt)))))
+
+(lcd-print ["This is a test." "Boing."])
 
 
 ; set up sync pin out
