caaacb62eac65be9d5ae64c467967c301723946f
Chris McCormick <chris@mccormick.cx>
2018-05-10 19:23:43 +0800

    Increase number of default channels.

--- src/xmp.cljs
+++ src/xmp.cljs
@@ -12,7 +12,10 @@
 
 (enable-console-print!)
 
-(def default-channels [1 1 1 1 1 1 1 1 1 1 1])
+(def default-channels [1 1 1 1
+                       1 1 1 1
+                       1 1 1 1
+                       1 1 1 1])
 
 (def edit-menu [:play-state :tick-freq :unmute-all])
 
