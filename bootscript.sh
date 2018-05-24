78c18d2688a267ccb9a49df32e6e3ae92f3044f1
Chris McCormick <chris@mccormick.cx>
2018-05-24 12:30:14 +0800

    Use wiringpi gpio cmd to show red boot light.

--- bootscript.sh
+++ bootscript.sh
@@ -2,6 +2,10 @@
 
 # Gets run from /opt/bootlocal.sh
 
+# turn on the LED so they know we're loading
+gpio -x mcp23017:100:0x20 mode 106 out
+gpio -x mcp23017:100:0x20 write 106 0
+
 # make sure mount points exist
 mkdir -p /mnt/usb
 # get into hardhat folder
